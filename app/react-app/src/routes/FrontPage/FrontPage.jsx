75eadc504b6cc10fdacff2e232f9f5982a93d951
Alexis Thierry <alexist103@gmail.com>
2024-04-16 16:57:11 +0200

    adding md on front page

--- app/react-app/src/routes/FrontPage/FrontPage.jsx
+++ app/react-app/src/routes/FrontPage/FrontPage.jsx
@@ -1,7 +1,23 @@
+import Markdown from 'react-markdown'
+import {useEffect, useState} from "react";
+import remarkGfm from 'remark-gfm'
+
+
+
 function FrontPage() {
+    const [markdown, setMarkdown] = useState('');
+
+    useEffect(() => {
+        fetch('../../README.md')
+            .then(response => response.text())
+            .then(text => setMarkdown(text));
+    }, []);
+
     return (
         <>
-        <div>Hello this is the front page</div>
+            <div>
+                <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+            </div>
         </>
     )
 }
