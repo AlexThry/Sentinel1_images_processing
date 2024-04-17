408de08dfe3246954cd3ba7596a3d1a21b1484ce
Adam jalil <johndoe@example.com>
2024-04-17 13:05:43 +0200

    [refactoring] separate asf_api in search.py et download.py

--- app/express-app/scripts/download.py
+++ app/express-app/scripts/download.py
@@ -11,36 +11,37 @@ if __name__ == "__main__":
     # Créer l'objet ArgumentParser
     parser = argparse.ArgumentParser(description='Télécharge des images Sentinel-1 sur ASF.')
     
-    parser.add_argument('--jsonList', nargs='+', help="Liste d'image à téléchargé")
+    parser.add_argument('--jsonFile', type=str, help="Path to the JSON file containing the list of images to download")
     parser.add_argument('--login', type=str, help='Login pour l\'authentification')
     parser.add_argument('--password', type=str, help='Password pour l\'authentification')
     
      # Parser les arguments
     args = parser.parse_args()
     
+        # Read the JSON file
+    with open(args.jsonFile, 'r') as f:
+        image_list = json.load(f)
+    
+    
     # Define the directory path
     dir_path = "../data/asf_set"
  
     # Créer la session
     session = asf.ASFSession().auth_with_creds(username=args.login, password=args.password)
-    jsonList=args.jsonList()
-    for image in jsonList:
-        # parse json
-        json_str = json.loads(str(image))
-        
+    for image in image_list["features"]:
          # Extract url
-        url = image["properties"]["url"]
-        
-        asf.download_urls(urls=args.urls, session=session, path = dir_path)
+        image_url = image["properties"]["url"]
+
+        asf.download_url(url=image_url, session=session, path = dir_path)
         
         # Extract the geometry from the S1Product
-        geometry = json_str['geometry'] # Replace with actual method
+        geometry = image['geometry'] # Replace with actual method
         # Convert the geometry to a shapely polygon
         polygon = shape(geometry)
-        timestamp = json_str["properties"]['processingDate']  # Replace with actual method
+        timestamp = image["properties"]['processingDate']  # Replace with actual method
 
         # Create old and new file paths
-        old_file_path = os.path.join(dir_path, json_str["properties"]["fileName"])
+        old_file_path = os.path.join(dir_path, image["properties"]["fileName"])
         new_file_path = os.path.join(dir_path, f'{timestamp}_{polygon}.zip')
         
         # Replace all spaces in new_file_path with no space
