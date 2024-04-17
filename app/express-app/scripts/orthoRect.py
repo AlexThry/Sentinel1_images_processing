bda3e1f0942d3c7f4a2a4ade8743121d5eef8c8e
cortesmc <carlosandres149@gmail.com>
2024-04-17 14:52:45 +0200

    [ADD]xml orthorectification py script

---
+++ app/express-app/scripts/orthoRect.py
@@ -0,0 +1,40 @@
+import argparse
+import traceback
+import json
+import os
+
+if __name__ == "__main__":    
+
+    xmlGraph = './OrthorectGraph_var.xml'
+    outputPath = "./newSubset/orthorect/"
+    snapExecutablePath = "gpt"
+
+    # Créer l'objet ArgumentParser
+    parser = argparse.ArgumentParser(description="Ortho rectification avec snap d'un image .dim.")
+
+    try:
+        # Ajouter les arguments
+        # parser.add_argument('--parameters', type=str, help="Path vers le dictionaire(json) des parameters pour l'interferogramme")
+        parser.add_argument('--input', type=str, help="Path pour l'image dim a orthorectifier")
+        parser.add_argument('--extension', type=str, help="Nom des bands generes à partir des l'interferogramme")
+
+        # Parser les arguments
+        args = parser.parse_args()
+
+        data_dict = {
+            "inputFile": args.input,
+            "outputFilePha": outputPath+args.extension+"_phase.tif",
+            "outputFileCoh": outputPath+args.extension+"_coh.tif",
+            "extension": args.extension
+        }
+        paramLine = ""
+        for k in data_dict.keys():
+            paramLine += f" -P{k}=\"{data_dict[k]}\""
+
+
+        finalCmd = f"{snapExecutablePath} {xmlGraph} {paramLine}"
+        os.system(f"{snapExecutablePath} {xmlGraph} {paramLine}")
+        
+
+    except Exception as e:
+        traceback.print_exc()
