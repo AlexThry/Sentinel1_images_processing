d89e2a35f2fd16feb7b69e10ecd53a46f041e86b
Alexis Thierry <alexist103@gmail.com>
2024-05-07 15:19:04 +0200

    Cleaning

--- app/react-app/readme.md
+++ app/react-app/readme.md
@@ -17,6 +17,8 @@ Il suffit de faire sa séléction sur la carte et de choisir les dates, puis de 
 Pour chaque image, on peut visualiser toutes les informations qui lui sont liées en cliquant sur "voir plus ->".
 
 Pour télécharger les images, il faut posséder un compte ASF et rentrer ses identifiants.
+
+
 ![Example Image](./Readme_assets/Download.jpeg)
 
 ## Traitement
@@ -25,10 +27,15 @@ Pour télécharger les images, il faut posséder un compte ASF et rentrer ses id
 
 Dans cette partie, on peut voir quelles images ont étées téléchargées et on peut effectuer un traitement sur ces images.
 Le traitement consiste pour l'instant seulement à un traitement des phases, mais pourrait étofé dans le futur.
+
+
 ![Example Image](./Readme_assets/Download.jpeg)
 
-## Visualistion
+## Visualisation
 
 ---
 
 Avec ce module vous pourrez afficher les images qui sont déja traitées. Vous pourrez choisir de les affichées en géomètrie sol et quelle bandes de l'interférogramme à affichée.
+
+
+![Example_Image](./Readme_assets/View.jpeg)
--- app/react-app/src/components/MainHeader/MainHeader.jsx
+++ app/react-app/src/components/MainHeader/MainHeader.jsx
@@ -9,12 +9,25 @@ function MainHeader(){
         <>
             <nav className={"navbar flex justify-between bg-white px-10"}>
                 <div className={"flex"}>
-                    <Link to={"/"} className={"btn text-xl"}>HOME</Link>
+                    <Link to={"/"} className={"btn text-xl"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="w-6 h-6">
+                            <path
+                                d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"/>
+                            <path
+                                d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z"/>
+                        </svg>
+
+
+                    </Link>
                 </div>
                 <div className={"flew gap-4"}>
-                    <Link to={"/download"} className={`btn btn-ghost ${currentPath === "/download" ? "bg-amber-50" : ""}`}>Download</Link>
-                    <Link to={"/process"} className={`btn btn-ghost ${currentPath === "/process" ? "bg-amber-50" : ""}`}>Process</Link>
-                    <Link to={"/view"} className={`btn btn-ghost ${currentPath === "/view" ? "bg-amber-50" : ""}`}>View</Link>
+                    <Link to={"/download"}
+                          className={`btn btn-ghost ${currentPath === "/download" ? "bg-amber-50" : ""}`}>Téléchargement</Link>
+                    <Link to={"/process"}
+                          className={`btn btn-ghost ${currentPath === "/process" ? "bg-amber-50" : ""}`}>Traitement</Link>
+                    <Link to={"/view"}
+                          className={`btn btn-ghost ${currentPath === "/view" ? "bg-amber-50" : ""}`}>Visualisation</Link>
                 </div>
             </nav>
         </>
